16d3ae14cc0485f575798019023d0b1e1a67b9b0
Shivam <beastboyshiv5@gmail.com>
2026-04-16 17:04:55 +0530

    wired jwtAuth and role middelware

--- user-management-backend/routes/auth.route.js
+++ user-management-backend/routes/auth.route.js
@@ -1,9 +1,14 @@
 import express from "express"
 import { login, register } from "../controllers/auth.controller.js";
+import { jwtAuth } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/role.middleware.js";
+
 const auth = express.Router();
+
+auth.use(jwtAuth)
 
 auth.post("/login",login)
 
-auth.post("/register",register)
+auth.post("/register",authorizeRoles(admin),register)
 
 export {auth}
