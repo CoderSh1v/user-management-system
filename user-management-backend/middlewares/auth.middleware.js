08462be9ce9102581acb4ece5bfd0c7ddb226c08
Shivam <beastboyshiv5@gmail.com>
2026-04-16 16:10:50 +0530

    added auth middleware

---
+++ user-management-backend/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+
+import jwt from "jsonwebtoken";
+
+export function jwtAuth(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({
+            success: false,
+            message: "Token Not found"
+        });
+    }
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_Secret);
+        req.user = {
+            userId: decoded.userId,
+            role: decoded.userRole
+        };
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized"
+        });
+    }
+}
