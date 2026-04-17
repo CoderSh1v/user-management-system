99cd364c68044988d48eb45800b48bceb405028b
Shivam <shivam.goyal.dev@gmail.com>
2026-04-17 14:18:50 +0530

    Fix server port configuration in server.js

--- user-management-backend/server.js
+++ user-management-backend/server.js
@@ -2,8 +2,8 @@ import {app} from "./app.js"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 dotenv.config()
-
+const port = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URI)
 
 
-app.listen(process.env.PORT)
+app.listen(port)
